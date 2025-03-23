f03501ac9258edeeb99bf90ed411d7b2b19ba7b0
airishGuy <airish1219here@gmail.com>
2025-03-23 17:40:15 +0100

    added client id

--- src/constants/client.js
+++ src/constants/client.js
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const clientId = "31f1b0e1a573a2d2fe506f90eb690aad";
 
 if (!clientId) {
   throw new Error("No client ID provided");
